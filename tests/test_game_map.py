e79871b081aebfb5e4a978815a647a6ccdfd5fd2
Devin O. Hall <orlandohall95@gmail.com>
2023-10-26 14:52:01 -0400

    added test_number_of_positions

--- tests/test_game_map.py
+++ tests/test_game_map.py
@@ -1,4 +1,14 @@
 from unittest import TestCase
-from levelup.GameMap import GameMap
+from levelup.gameMap import GameMap
 
 class TestGameMap(TestCase):
+    def test_number_of_positions(self):
+        # Create an instance of the GameMap
+        gameMap = GameMap()
+        
+        # Call the method that returns the number of positions
+        num_positions = gameMap.num_positions
+        
+        # Check if the number of positions is 100
+        self.assertEqual(num_positions, 100, "Number of positions should be 100")
+
