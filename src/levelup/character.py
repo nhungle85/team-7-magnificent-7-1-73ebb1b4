bff67bda9e3a6da0934124a5b7f8cc0c94b7979d
Jason <jelmore88@gmail.com>
2023-10-26 14:35:40 -0400

    add is valid name test case

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,5 +1,25 @@
+import re
+
+def is_valid_name(name):
+        # Check if the name contains only letters and is no more than 15 characters.
+        pattern = r"^[a-zA-Z]{1,15}$"
+        
+        return bool(re.match(pattern, name))
 class Character:
     name = ""
 
     def __init__(self, character_name):
-        self.name = character_name
+        if is_valid_name(character_name):
+            self.name = character_name
+        else:
+            self.name = ""
+
+        # # Test the function with some example names
+        # names = ["John", "Alice123", "Elizabeth Ann", "Bob_"]
+
+        # for name in names:
+        #     if is_valid_name(name):
+        #         print(f"{name} is a valid name.")
+        #     else:
+        #         print(f"{name} is not a valid name.")
+
--- tests/test_character.py
+++ tests/test_character.py
@@ -1,8 +1,15 @@
 from unittest import TestCase
-from levelup.character import Character
+from levelup.character import *
 
 class TestCharacterInitWithName(TestCase):
     def test_init(self):
         ARBITRARY_NAME = "MyName"
         testobj = Character(ARBITRARY_NAME)
         self.assertEqual(ARBITRARY_NAME, testobj.name)
+
+  
+    def test_is_valid_name(self):
+        ARBITRARY_NAME = "MyName@"
+        expected = is_valid_name(ARBITRARY_NAME)
+       
+        self.assertFalse(expected)
