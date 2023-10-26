46f6ca58e45b6e07990d2fb1814c8680d98417e2
Devin O. Hall <orlandohall95@gmail.com>
2023-10-26 15:15:39 -0400

    added test_get_positions

--- tests/test_game_map.py
+++ tests/test_game_map.py
@@ -4,7 +4,7 @@ from levelup.gameMap import GameMap
 class TestGameMap(TestCase):
     def test_number_of_positions(self):
         # Create an instance of the GameMap
-        gameMap = GameMap()
+        game_map = GameMap()
         
         # Call the method that returns the number of positions
         num_positions = gameMap.num_positions
@@ -12,3 +12,14 @@ class TestGameMap(TestCase):
         # Check if the number of positions is 100
         self.assertEqual(num_positions, 100, "Number of positions should be 100")
 
+class TestGameMap(TestCase):
+    def test_get_character_position(self):
+        # Create an instance of the GameMap
+        game_map = GameMap()
+        
+        # Assume you have placed a character at position (3, 4)
+        character_position = game_map.get_character_position(3, 4)
+        
+        # Check if the returned position matches the expected position
+        expected_position = "Character found at position (3, 4)"
+        self.assertEqual(character_position, expected_position, "Position of character is incorrect")
